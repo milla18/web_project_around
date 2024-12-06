c679d683c7082755dcca359c16d293ceb4457fbc
Camila <camila.lc1801@gmail.com>
2024-12-06 09:07:07 -0300

    Adiciona novos arquivos e ajustes

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Tripleten web_project_around
+
+JavaScript Básico e Trabalho com o DOM
+
+# Projeto Web Responsivo
+
+Este projeto demonstra como utilizar JavaScript para melhorar a experiência do usuário ao interagir com formulários em páginas web. Com foco em validação de dados e manipulação de eventos, o objetivo é garantir que os usuários insiram informações corretas e completas, evitando erros e frustrações durante a navegação.
+
+# Ferramentas
+
+DevTools, VSCode e W3C Markup Validator para validar html,css,javaScript,git,github e figma garantindo que o código esteja de acordo com os padrões web.
+
+# Aprendizado
+
+Durante o desenvolvimento deste projeto, aprendi e desenvolvi as seguintes habilidades:
+Manipulação de HTML e CSS com JavaScript Manipulação de Objetos em JavaScript, Manipulação de Eventos, Melhoria da Experiência do Usuário Validação de Formulários o conteúdo e o estilo de uma página. Desenvolvimento Responsivo:Criação de layouts que se adaptam a diferentes tamanhos de tela e dispositivos.
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-function salvar(event) {
-  alert("Alteração feita!");
+document.querySelector("form").addEventListener("submit", function (event) {
   event.preventDefault();
 
   //   aqui estão pegando os valores do input do usuario
@@ -10,13 +9,221 @@ function salvar(event) {
   document.querySelector(".profile__paragraph").textContent =
     bibliografia.value;
 
-  fechar();
-}
+  closing();
+});
 
-function editar() {
+function edit() {
   document.querySelector(".edit__forms").style.display = "flex";
 }
 
-function fechar() {
+function closing() {
   document.querySelector(".edit__forms").style.display = "none";
+}
+
+function openFormAdd() {
+  document.querySelector(".form__add").style.display = "flex";
+}
+
+//fechar add
+function closed() {
+  document.querySelector(".form__add").style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const initialCards = [
+    {
+      name: "Vale de Yosemite",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+    },
+    {
+      name: "Lago Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+    },
+    {
+      name: "Montanhas Carecas",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+    },
+    {
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+    },
+    {
+      name: "Parque Nacional da Vanoise ",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+    },
+  ];
+  initialCards.forEach(createCard);
+  // variaveis dos cartoes
+  let cardscontainer = document.querySelector(".gallery");
+  // variaveis dos forms
+  let formAdd = document.querySelector(".second__form");
+  let form = document.querySelector(".form__add");
+
+  //Background escuro no card hover
+
+  cardscontainer.addEventListener("click", function (event) {
+    if (event.target.classList.contains("card__image")) {
+      let imgSrc = event.target.src;
+      selectImg.src = imgSrc;
+      select.style.display = "flex";
+      let card = event.target.closest(".card");
+      let cardTitle = card.querySelector(".card__text").textContent;
+      selectText.textContent = cardTitle;
+
+      let selectValue = document.querySelector(".content");
+      selectValue.classList.add("page__opacity");
+    }
+  });
+
+  // fechamento do form e adicionado os cartoes
+  function addCard(event) {
+    event.preventDefault();
+    let name = document.querySelector("#titulo").value;
+    let link = document.querySelector("#link").value;
+    if (name && link) {
+      createCard({ name, link });
+      formAdd.reset();
+    }
+    closed();
+  }
+
+  // codigo para clicar no +
+
+  formAdd.addEventListener("submit", addCard);
+
+  //lixeira
+  cardscontainer.addEventListener("click", function (event) {
+    if (event.target.classList.contains("card__trash")) {
+      let card = event.target.closest(".card");
+      card.remove();
+    }
+  });
+
+  //coração
+  cardscontainer.addEventListener("click", function (event) {
+    if (
+      event.target.classList.contains("heart__white") ||
+      event.target.classList.contains("heart__black")
+    ) {
+      event.target.classList.toggle("heart__white");
+      event.target.classList.toggle("heart__black");
+    }
+  }); //adicionando os cartoes
+  function createCard(card) {
+    let cardTemplate = document.querySelector("#cards").content;
+    let cardscontainer = document.querySelector(".gallery");
+    let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+    const cardTrash = cardElement.querySelector(".card__trash");
+    const cardImage = cardElement.querySelector(".card__image");
+    const cardText = cardElement.querySelector(".card__text");
+    const cardHeart = cardElement.querySelector(".heart__white");
+
+    cardTrash.setAttribute("role", "button");
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
+
+    cardText.textContent = card.name;
+    cardscontainer.prepend(cardElement);
+  }
+
+  // variaveis do card popup img e title
+  let select = document.querySelector(".card__popup");
+  let selectImg = document.querySelector(".imagem__popup");
+  let selectText = document.querySelector(".title___popup");
+  // fazendo as modificaçoes de card popup img e title
+  cardscontainer.addEventListener("click", function (event) {
+    if (event.target.classList.contains("card__image")) {
+      let imgSrc = event.target.src;
+      selectImg.src = imgSrc;
+      select.style.display = "flex";
+      let card = event.target.closest(".card");
+      let cardTitle = card.querySelector(".card__text").textContent;
+      selectText.textContent = cardTitle;
+    }
+  });
+});
+
+//adicionando os cartoes
+function createCard(card) {
+  let cardTemplate = document.querySelector("#cards").content;
+  let cardscontainer = document.querySelector(".gallery");
+  let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  const cardTrash = cardElement.querySelector(".card__trash");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardText = cardElement.querySelector(".card__text");
+  const cardHeart = cardElement.querySelector(".heart__white");
+
+  cardTrash.setAttribute("role", "button");
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+
+  cardText.textContent = card.name;
+  cardscontainer.prepend(cardElement);
+}
+
+// fechar img dos cartões
+function closures() {
+  document.querySelector(".card__popup").style.display = "none";
+  document.querySelector(".content").classList.remove("page__opacity");
+}
+
+// esc e apertar fora
+closingPopup({
+  formSelector: ".edit__forms",
+  formSelectorAdd: ".form__add",
+  formSelectorCard: ".card__popup",
+  form: ".form",
+});
+
+function closingPopup(config) {
+  const formElement = document.querySelectorAll(config.form);
+  const popups = [
+    document.querySelector(config.formSelector),
+    document.querySelector(config.formSelectorAdd),
+    document.querySelector(config.formSelectorCard),
+  ];
+
+  // fechamento do poup o if para tirar o escuro da pag
+  function closepopup(popup) {
+    if (popup.classList.contains("card__popup")) {
+      document.querySelector(".content").classList.remove("page__opacity");
+    }
+    popup.style.display = "none";
+  }
+
+  //validar o estilo e chamar fechamento do poup
+  function closePopups() {
+    popups.forEach((popup) => {
+      if (popup && getComputedStyle(popup).display !== "none")
+        closepopup(popup);
+    });
+  }
+
+  // fechamento com esc
+  function handleEscape(event) {
+    if (event.key == "Escape" && isAnyPopupVisible()) closePopups();
+  }
+
+  // validaçao dos poup se for clicado do lado de fora
+  function handleClickOutside(event) {
+    const clickedInsideAnyPopup = Array.from(formElement).some((form) =>
+      form.contains(event.target)
+    );
+    if (!clickedInsideAnyPopup && isAnyPopupVisible()) closePopups();
+  }
+  // Check if any popup is visible
+  function isAnyPopupVisible() {
+    return popups.some(
+      (popup) => popup && getComputedStyle(popup).display === "flex"
+    );
+  }
+
+  document.addEventListener("keydown", handleEscape);
+  document.addEventListener("mousedown", handleClickOutside);
 }
